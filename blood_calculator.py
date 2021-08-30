504432f1946d33fdeaa6fb619d4b2db54d5af4c4
Bidisha Nandi <bn74@duke.edu>
2021-08-30 12:52:42 -0400

    Fixed code string stuff

--- blood_calculator.py
+++ blood_calculator.py
@@ -1,9 +1,13 @@
 def interface():
-    print("Blood Calculator")
-    print("Make a choice")
-    print("9-quit")
-    choice = input("Make a choice: ")
-    print(choice)
-    return choice
+	print("Blood Calculator")
+	keep_running = True
+	while keep_running:
+		print("Make a choice")
+		print("9-quit")
+		choice =int( input("Make a choice: "))
+		if choice == 9:
+       			keep_running = False
+	print(choice)
+	return choice
 
 interface()
